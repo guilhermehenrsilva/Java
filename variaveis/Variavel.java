13cb8951623c802e071eab4418a9d9bf7824f561
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2025-02-27 11:05:41 -0300

    Atualizado - Variaveis

---
+++ variaveis/Variavel.java
@@ -0,0 +1,16 @@
+package variaveis;
+
+public class Variavel {
+    public static void main(String[] args) {
+        // 1 - o que são variaveis 
+        // tipo --> nome --> tribuir um valor 
+        
+        String nome = "Guilherme";
+        String idade = "27 anos" ;
+
+        // nome => "Guilherme"
+        System.out.println(nome);
+        System.out.println(idade);
+       // System.out.println("Guilherme");
+    }
+}
